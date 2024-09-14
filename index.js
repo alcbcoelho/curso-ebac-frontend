420dd68bc9b60157f733f242cb2e668b2c0d3e30
André Coêlho <alcbcoelho@gmail.com>
2024-09-14 02:08:04 -0300

    Feito exercício do módulo 'Recursos do ES6+'

--- index.js
+++ index.js
@@ -1,41 +1,50 @@
-const Veiculo = function(modelo, marca, anoFabricacao) {
-    this.modelo = modelo;
-    this.marca = marca;
-    this.anoFabricacao = anoFabricacao;
+const alunos = [
+    {
+        nome: "Rogério",
+        nota: 7,
+    },
+    {
+        nome: "Martha",
+        nota: 8,
+    },
+    {
+        nome: "Lucas",
+        nota: 4,
+    },
+    {
+        nome: "Rosângela",
+        nota: 6,
+    },
+    {
+        nome: "Astolfo",
+        nota: 9,
+    },
+    {
+        nome: "Priscilla",
+        nota: 10,
+    },
+    {
+        nome: "Gastão",
+        nota: 3,
+    },
+    {
+        nome: "Letícia",
+        nota: 5,
+    },
+    {
+        nome: "Diógenes",
+        nota: 9,
+    },
+    {
+        nome: "Vanessa",
+        nota: 0,
+    },
+]
+
+function exibirAlunosComNotaMaiorOuIgualA6(arr = alunos) {
+    return arr.filter(aluno => aluno.nota >= 6);
 }
 
-const Carro = function(modelo, marca, anoFabricacao) {
-    Veiculo.call(this, modelo, marca, anoFabricacao);
-}
-
-Carro.prototype.acelerar = function() {
-    console.log(`${this.modelo} acelera: "VROOOM"`);
-}
-
-const Bicicleta = function(modelo, marca, anoFabricacao) {
-    Veiculo.call(this, modelo, marca, anoFabricacao);
-}
-
-Bicicleta.prototype.pedalar = function() {
-    console.log(`${this.modelo} pedala!`);
-}
-
-const carros = [
-    new Carro("Corolla", "Toyota", 2022),
-    new Carro("Mustang", "Ford", 2021),
-    new Carro("Golf", "Volkswagen", 2023)
-];
-
-const bicicletas = [
-    new Bicicleta("Rockhopper", "Specialized", 2022),
-    new Bicicleta("Domane AL 2", "Trek", 2023),
-    new Bicicleta("Trail 7", "Cannondale", 2021)
-];
-
-carros.forEach(carro => {
-    carro.acelerar();
-})
-
-bicicletas.forEach(bicicleta => {
-    bicicleta.pedalar();
-})
+console.log(
+    exibirAlunosComNotaMaiorOuIgualA6()
+);
